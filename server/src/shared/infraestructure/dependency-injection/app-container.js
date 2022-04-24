14ff8b59cb5ffff67f41e1edd574fe1a435ba18c
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-04-24 00:49:21 -0500

    feat: add app container

---
+++ server/src/shared/infraestructure/dependency-injection/app-container.js
@@ -0,0 +1,29 @@
+import { ContainerBuilder } from 'node-dependency-injection';
+import { setSharedModule } from './shared.module.js';
+import { setResourcesModule } from '../../../resources/infraestructure/dependency-injection/resources.module.js';
+
+export class AppContainer {
+  static #instace;
+  #container;
+
+  constructor() {
+    this.#container = new ContainerBuilder();
+  }
+
+  static getInstance() {
+    if(!AppContainer.#instace) {
+      AppContainer.#instace = new AppContainer();
+    }
+
+    return AppContainer.#instace;
+  }
+
+  getContainer() {
+    return this.#container;
+  }
+
+  setDependencyInjectionApp() {
+    setSharedModule(this.#container);
+    setResourcesModule(this.#container);
+  }
+}
